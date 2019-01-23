ae874eb8403b2f50a8bdc509fc1b951807b9f763
Bruno dos Santos <brunowsan@gmail.com>
2019-01-23 13:01:30 -0200

    Atualização do código

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ async def on_ready():
 
 
 @client.command(pass_context=True)
-async def teste(ctx)
+async def teste(ctx):
     await client.say("```++ Time Machine: Active! ++```")
     await asyncio.sleep(60*60*7.5)
     await client.say("```++ Time Machine: Running low on gas ++```")
-    await asyncio.sleep(60*60*8)
+    await asyncio.sleep(60*30)
     await client.say("```++ Time Machine: Deactivate ++```")
 
 
-client.run(NTM3NDg0ODcxMjUwMDgzODQw.DyoO5g.am-NvQVAe31YWzOcDR2vbIJaHM8)
+client.run('NTM3NDg0ODcxMjUwMDgzODQw.DyoO5g.am-NvQVAe31YWzOcDR2vbIJaHM8')
